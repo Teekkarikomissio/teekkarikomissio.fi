1ec13da59a54e3871353322d85270e725c634138
rkantero <jrkantero@gmail.com>
2021-08-18 17:05:31 +0300

    testing juhani

--- static-translations/locales/fi/association.js
+++ static-translations/locales/fi/association.js
@@ -3,4 +3,12 @@ export default {
   associationHeading: 'Mitä TK oikeastaan tekee?',
   associationContent:
     'Teekkarikomissio hoitaa jäsenyhdistyksilleen yhteisiä asioita, edustaa Turun teekkareita ylioppilaskunta-tasolla ja vastaa heidän korkeakoulupoliittisista kannanotoista. Lisäksi Komissio on tärkeä yhdyskanava Turussa toimiville teekkariyhdistyksille. Käytännössä Teekkarikomission järjestämään toimintaan kuuluu hallituksen ja yhdistyksen kokouksia, edustusmatkoja sekä enemmän tai vähemmän säännöllisiä tapahtumia.',
-};
+  associationHeading1: 'Vuoden',
+  associationHeading2: 'hallitus',
+  associationTitle: 'Pääkopmissaari',
+  associationTitle1: 'Vara pääkomissaari',
+  associationTitle2: 'Sihteeri ja viestintäkomissaari',
+  associationTitle3: 'Taalouskomissaari',
+  associationTitle4: 'Tapahtumakomissaari',
+  associationTitle5: 'Kulttuurikomissaari',
+  };
