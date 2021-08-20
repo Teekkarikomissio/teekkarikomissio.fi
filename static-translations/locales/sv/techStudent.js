c84e04209dd47d8d38d56cdee4cc572e810ab3ff
rkantero <jrkantero@gmail.com>
2021-08-20 18:38:47 +0300

    translate guildcards in association

--- static-translations/locales/sv/techStudent.js
+++ static-translations/locales/sv/techStudent.js
@@ -5,4 +5,11 @@ export default {
   techStudentHeading1: 'Medlemsföreningar',
   techStudentBody1: 'Teknologkommissionen har fyra medlemmar, nämligen de teknologföreningar som har sin verksamhet i Åbo. Varje röstberättigad medlem av Digit ry, Nucleus ry, Datateknologerna vid Åbo Akademi rf och Kemistklubben vid Åbo Akademi rf är alltså automatiskt berättigad att delta i TK:s verksamhet!',
   techStudentHeading2: 'Alumnföreningar',
+  techStudentInfo: 'Datateknik',
+  techStudentInfo1: 'Data- och kommunikationsteknik',
+  techStudentInfo2: 'Kemi- och processteknik',
+  techStudentInfo3: 'Bioteknik och livsmedelsutveckling',
+  techStudentInfo4: 'Turun DI-kerho har till syfte att fungera som ett band mellan TEK:s medlemmar i området.',
+  techStudentInfo5: '0-kerho är Digit ry:s alumniförening och har som ändamål att fungera som en länk mellan nya och gamla studerande.',
+  techStudentInfo6: 'Akademiska kvinnor inom tekniken i Egentliga Finland',
 };
