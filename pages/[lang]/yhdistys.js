4fcf96690bc98d6a5f2a69967dc0c7dbccfb258b
rkantero <jrkantero@gmail.com>
2021-09-02 09:57:46 +0300

    test 3

--- pages/[lang]/yhdistys.js
+++ pages/[lang]/yhdistys.js
@@ -65,7 +65,7 @@ const Yhdistys = () => {
             <H2>{name}</H2>
             <p className="text-gray-700 text-lg my-4">{position}</p>
             <p className="text-gray-700 text-base">{t('associationOtherResponsibilities')} {responsibilities}</p>
-            <p className="text-gray-700 text-lg my-4">{contact}</p>
+            <p className="text-gray-700 text-base">{contact}</p>
           </div>
         </div>
       </div>
