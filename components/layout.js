eaee34bf98cfc9a826f3e57e0f2625b9f5fb0c97
Miika Peltotalo <miika@identio.fi>
2023-05-13 02:51:12 +0300

    Netlify has an annoying case sensitivity bug and this will fix it

---
+++ components/layout.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Head from "next/head";
+
+import Navbar from "./Navbar";
+import Footer from "./footer";
+
+const Layout = ({ titleKey, children }) => {
+  return (
+    <div className="flex flex-col text-center">
+      <Head>
+        <title>{titleKey}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Teekkarikomissio (TK) on yhteensitova kontaktifoorumi Turussa toimiville teekkariyhdistyksille."
+        />
+        <meta
+          name="keywords"
+          content="teekkarikomissio, komissio, teknologkommissionen, kommissionen, teekkari, turku, tekniikka, opiskelu, yliopisto, tietotekniikka, biotekniikka, materiaalitekniikka, konetekniikka, fuksi, paavo, nurmi, vappu, wappu, lakitus, teekkariutta, turkulaista, TK, tk, teekkarikulttuuri, jäynäkulttuuri, teekkarilakki, yhdistys, fukseille, kulttuuri, yrityksille, fuksipassi, lakinkäyttöoikeus, käyttöoikeus, pysyväisohjesääntö, eldprowet, jäynäkilpailu"
+        />
+        <meta name="author" content="Teekkarikomissio - Teknologkommissionen" />
+        <meta property="og:image" content="/index-banner-fi.jpg" />
+        <link rel="shortcut icon" sizes="32x32" href="/tklogo.svg" />
+      </Head>
+      <Navbar />
+      <div className="flex-expand flex flex-col items-center max-h-full">
+        <div className="container mx-auto max-w-prose max-h-full">
+          {children}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
