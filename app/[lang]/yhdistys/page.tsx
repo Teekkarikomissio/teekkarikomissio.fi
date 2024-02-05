675a5ba983a1ef71bc8904424be0ba18394b923b
Ellen Ekblom <ellen.ekblom@gmail.com>
2024-02-05 22:51:20 +0200

    add image for Mikko

--- app/[lang]/yhdistys/page.tsx
+++ app/[lang]/yhdistys/page.tsx
@@ -15,6 +15,7 @@ import Tiitus from "/public/board/2024/tiitus-hannula-min.jpg"
 import Kristoffer from "/public/board/2024/kristoffer-lindholm-min.jpg"
 import Arttu from "/public/board/2024/arttujokinen-min.jpg"
 import Marianne from "/public/board/2024/marianne-matinvuori-min.jpg"
+import Mikko from "/public/board/2024/mikko-Lehtosalo-min.jpg"
 import { BoardCard } from "@/types";
 
 export default async function Yhdistys({
@@ -83,7 +84,7 @@ export default async function Yhdistys({
       contact2: "TG: @rroskaa",
     },
     {
-      img: tklogo,
+      img: Mikko,
       name: "Mikko Lehtosalo",
       position: association.associationTitle4,
       //responsibilities: association.associationResponsibilities7,
