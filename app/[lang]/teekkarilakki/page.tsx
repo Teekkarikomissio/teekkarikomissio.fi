c264cde3d4a1727f650ae30163d82525def692b4
Miika Peltotalo <miika@identio.fi>
2024-08-17 14:36:02 +0300

    Made teekkarilakki picture bigger

--- app/[lang]/teekkarilakki/page.tsx
+++ app/[lang]/teekkarilakki/page.tsx
@@ -1,26 +1,24 @@
-import React from "react";
+import React from 'react'
 
-import { Locale } from "../../../i18n-config";
+import { Locale } from '../../../i18n-config'
 import getPageBySlug from '@/lib/api'
 import markdownToHtml from '@/lib/markdownToHtml'
-import markdownStyles from '../../../components/markdown-styles.module.css'
+import teekkarilakkiStyles from './teekkarilakki-styles.module.css'
 
 export default async function Cap({
   params: { lang },
 }: {
-  params: { lang: Locale };
+  params: { lang: Locale }
 }) {
   const capPage = getPageBySlug('teekkarilakki/teekkarilakki', lang)
   const content = await markdownToHtml(capPage.content || '')
 
   return (
-    <div className="max-w-prose">
-      <div className="max-w-2xl mx-auto">
-        <div
-          className={markdownStyles['markdown']}
-          dangerouslySetInnerHTML={{ __html: content }}
-        />
-      </div>
+    <div className="max-w-prose mx-auto">
+      <div
+        className={teekkarilakkiStyles['markdown']}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </div>
-  );
+  )
 }
