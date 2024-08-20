23e35ce2194dc2d73e2e33d503a5208596daed62
Miika Peltotalo <miika@identio.fi>
2024-08-20 23:33:44 +0300

    Moved last pieces of content to markdown

---
+++ _content/dokumentit/dokumentit.sv.md
@@ -0,0 +1,13 @@
+---
+title: Dokument
+---
+
+![Liput](/tk-banner.jpg)
+
+## Teknologkommissionens officiella dokument
+
+- Stadgar: [På Finska](/rules-fi.pdf) [På Svenska](/rules-sv.pdf)  
+- Arbetsordning: [På Finska](/sub-rules-fi.pdf) [På Svenska](/sub-rules-sv.pdf)  
+- Strategi: [På Finska](/strategy-fi.pdf) [På Svenska](/strategy-sv.pdf)  
+- Teknologhymnen: [Noter](/teekkarihymni_teknologhymn_notes.pdf) [Ord](/teekkarihymni_teknologhymn_words.pdf)  
+- Jämställdhetsplan: [På Finska](/values-fi.pdf) Väntar på översättning  
--- app/[lang]/yrityksille/page.tsx
+++ app/[lang]/yrityksille/page.tsx
@@ -1,37 +1,24 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react'
 
-import { Locale } from "../../../i18n-config";
-import { getDictionary } from "../../../get-dictionary";
-import { H1, LongText, ListItem } from "../../../components/Typography";
-
-import paavo from "../../../public/yrityksille-paavo.jpg"
+import { Locale } from '../../../i18n-config'
+import getPageBySlug from '@/lib/api'
+import markdownToHtml from '@/lib/markdownToHtml'
+import yrityksilleStyles from './yrityksille-styles.module.css'
 
 export default async function ForCompanies({
-    params: { lang },
-  }: {
-    params: { lang: Locale };
-  }) {
-    const dictionary = await getDictionary(lang);
-    const forCompanies = dictionary["forCompanies"];
-
+  params: { lang },
+}: {
+  params: { lang: Locale }
+}) {
+  const capPage = getPageBySlug('yrityksille/yrityksille', lang)
+  const content = await markdownToHtml(capPage.content || '')
 
   return (
-    <div className="max-w-prose">
-      <Image src={paavo} alt="Paavon lakitus" />
-      <H1>{forCompanies.forCompaniesHeading}</H1>
-      <LongText>{forCompanies.forCompaniesBody}</LongText>
-      <ul className="list-decimal mb-8 mx-16">
-        <ListItem>{forCompanies.forCompaniesListItem}</ListItem>
-        <ListItem>{forCompanies.forCompaniesListItem1}</ListItem>
-        <ListItem>{forCompanies.forCompaniesListItem2}</ListItem>
-      </ul>
-      <LongText>
-        {forCompanies.forCompaniesBody1}{" "}
-        <a className="underline" href="mailto:teekkarikomissio@utu.fi">
-          teekkarikomissio[@]utu.fi
-        </a>
-      </LongText>
+    <div className="max-w-prose mx-auto">
+      <div
+        className={yrityksilleStyles['markdown']}
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </div>
-  );
-};
+  )
+}
